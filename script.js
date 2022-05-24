ea4e541288f499c9cc348cea2125a86f34774105
sangminlee98 <tkdals0975@naver.com>
2022-05-24 17:58:43 +0900

    init

---
+++ script.js
@@ -0,0 +1,21 @@
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+const body = document.querySelector('body');
+
+let inputValue = '';
+
+btn.addEventListener('click', () => {
+  inputValue = input.value;
+  const containerElem = document.createElement('div');
+  const textElem = document.createElement('span');
+  const deleteElem = document.createElement('button');
+  deleteElem.setAttribute('class','deleteBtn');
+  deleteElem.innerText = '삭제';
+  textElem.textContent = inputValue;
+  containerElem.appendChild(textElem);
+  containerElem.appendChild(deleteElem);
+  body.appendChild(containerElem);
+  input.value = '';
+})
+
+
